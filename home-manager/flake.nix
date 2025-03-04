f3c85af30b4e71b9ae2e02116d9c391d7b8d69ca
Jhuan-Nycolas <jhuan2024@hotmail.com>
2025-03-04 17:32:37 -0300

    recuperando home manager

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -15,18 +15,15 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      tux = {
-        Plasma = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
+      homeConfigurations."tux" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
 
-          modules = [ ./hosts/plasma/home.nix ];
-        };
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
 
-        OpenBox = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-
-          modules = [ ./hosts/openbox/home.nix ];
-        };
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
     };
-  };
 }

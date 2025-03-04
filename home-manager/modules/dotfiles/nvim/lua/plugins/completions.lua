3098bb0353163f5b546ab068e74e7290de375b38
Jhuan-Nycolas <jhuan2024@hotmail.com>
2025-03-04 14:35:46 -0300

    Fixing Friendly Snippets bug

--- home-manager/modules/dotfiles/nvim/lua/plugins/completions.lua
+++ home-manager/modules/dotfiles/nvim/lua/plugins/completions.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets"
+    },
+  },
+
+  {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require("cmp")
@@ -30,13 +38,5 @@ return {
         }),
       })
     end,
-  },
-
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets"
-    },
   }
 }

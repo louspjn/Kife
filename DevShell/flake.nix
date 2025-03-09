7d00050f89dd722cb91e3ee492bd9b4db97c46c3
Jhuan-Nycolas <jhuan2024@hotmail.com>
2025-03-08 21:33:50 -0300

    Mudando o nix develop de lugar

---
+++ DevShell/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Ambientes de desenvolvimento Java e Maven";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system} = {
+      Maven = pkgs.mkShell {
+        buildInputs = [
+          pkgs.openjdk
+          pkgs.maven
+        ];
+        
+        shellHook = "exec fish";
+      };
+
+      Yarn = pkgs.mkShell {
+        buildInputs = [
+          pkgs.yarn
+        ];
+
+        shellHook = "exec fish";
+      };
+    };
+  };
+}
+

4d68933810dea579b5d02f6450a0279b66ae4a75
Jhuan-Nycolas <jhuan2024@hotmail.com>
2025-03-10 22:56:24 -0300

    Mudando uns aliases para funcionarem corretamente

--- home-manager/hosts/default/fish.nix
+++ home-manager/hosts/default/fish.nix
@@ -13,10 +13,10 @@
       alias cd='z'
       alias cat='bat'
       alias nvim='nix run ~/.config/system/Flakes/nvim/'
-      alias hw='home-manager switch --flake ~/.config/system/home-manager'
-      alias nw='sudo nixos-rebuild switch --flake ~/.config/system/nixos'
+      alias hw='home-manager switch --flake'
+      alias nw='sudo nixos-rebuild switch --flake'
       alias sysconf='cd ~/.config/system/; nvim'
-      alias devshell='nix develop --impure ~/.config/system/DevShell'
+      alias devshell='nix develop --impure'
       
       # Ls
       alias ls='exa'

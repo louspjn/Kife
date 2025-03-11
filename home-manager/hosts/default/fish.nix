897460e48e2588a05b4c4efc9268f064abfbb51a
Jhuan-Nycolas <jhuan2024@hotmail.com>
2025-03-10 22:52:54 -0300

    Configurando uns aliases no fish

--- home-manager/hosts/default/fish.nix
+++ home-manager/hosts/default/fish.nix
@@ -12,8 +12,11 @@
       # Alias
       alias cd='z'
       alias cat='bat'
-      alias ficg='nvim ~/.config/system-config/home-manager/modules/fish.nix'
-      alias nvf='nix run ~/.config/system/Flakes/nvim/'
+      alias nvim='nix run ~/.config/system/Flakes/nvim/'
+      alias hw='home-manager switch --flake ~/.config/system/home-manager'
+      alias nw='sudo nixos-rebuild switch --flake ~/.config/system/nixos'
+      alias sysconf='cd ~/.config/system/; nvim'
+      alias devshell='nix develop --impure ~/.config/system/DevShell'
       
       # Ls
       alias ls='exa'

a23a5ebf8be8ec8ea40c613edcbe1db7e003f551
Jhuan-Nycolas <jhuan2024@hotmail.com>
2025-03-12 20:55:23 -0300

    usando a config do neovim do github

--- home-manager/hosts/default/fish.nix
+++ home-manager/hosts/default/fish.nix
@@ -12,11 +12,11 @@
       # Alias
       alias cd='z'
       alias cat='bat'
-      alias nvim='nix run ~/.config/system/Applications/nvim/'
+      alias nvim='nix run github:Jhuan-Nycolas/Nvim'
       alias hw='home-manager switch --flake'
       alias nw='sudo nixos-rebuild switch --flake'
       alias sysconf='cd ~/.config/system/; nvim'
-      alias devshell='nix develop --impure'
+      alias devshell='nix develop'
       
       # Ls
       alias ls='exa --icons'

4a93c0d596ccf85801f6c49a69f0863973e1f5f4
Jhuan-Nycolas <jhuan2024@hotmail.com>
2025-03-16 00:11:03 -0300

    Configurando o fastfetch

--- home-manager/hosts/default/fastfetch.nix
+++ home-manager/hosts/default/fastfetch.nix
@@ -1,5 +1,36 @@
 {
   programs.fastfetch = {
     enable = true;
+
+    settings = {
+      logo = {
+        #source = "~/.config/system/home-manager/hosts/images/NixOS-Fastfetch.png";
+
+        width = 30;
+        height = 12;
+      };
+      display = {
+        size = {
+          binaryPrefix = "si";
+        };
+        color = "blue";
+        separator = "  ";
+      };
+      modules = [
+        {
+          type = "datetime";
+          key = "Date";
+          format = "{1}-{3}-{11}";
+        }
+        {
+          type = "datetime";
+          key = "Time";
+          format = "{14}:{17}:{20}";
+        }
+        "break"
+        "player"
+        "media"
+      ];
+    };
   };
 }

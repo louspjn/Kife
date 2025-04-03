7a3f066b4e1104e006348e977a7cc7a66dc8c7dc
Jhuan-Nycolas <jhuan2024@hotmail.com>
2025-04-03 11:09:20 -0300

    Adicionando mais paths ao reop

--- home-manager/hosts/default/reop.nix
+++ home-manager/hosts/default/reop.nix
@@ -5,6 +5,11 @@
       editor = "nix run github:Jhuan-Nycolas/Nvim";
       projects = {
         Option.path = "~/Option/";
+        Web.path = "~/Dev/web";
+        Dev.path = "~/Dev/";
+        Cpp.path = "~/Dev/Cpp";
+        Python.path = "~/Dev/Python/";
+        Rust.path = "~/Dev/Rust/";
       };
     };
   };

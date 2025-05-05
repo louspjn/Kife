6a3327d37e6bc3b3937499c95253d427a7428d2b
haskex <jhuan2024@hotmail.com>
2025-05-04 23:02:59 -0300

    Acho que a waybar ta pronta

--- config/hyprland/waybar.nix
+++ config/hyprland/waybar.nix
@@ -11,7 +11,7 @@
 
         modules-left = ["custom/logo" "hyprland/window"];
         modules-center = ["hyprland/workspaces"];
-        modules-right = ["clock" "custom/lock"];
+        modules-right = ["network" "clock" "battery"];
 
         "custom/logo" = {
           format = " ";
@@ -28,7 +28,66 @@
             "*" = [2 3 4 5 6];
           };
         };
+
+        "network" = {
+          format-wifi = " ";
+          format-disconnected = " ";
+
+          tooltip-format-wifi = "{essid} ({signalStrength}%)  ";
+          tooltip-format-disconnected = "Disconnected  ";
+        };
+
+        "clock" = {
+          format = "{:%I:%M %p}";
+          interval = 5;
+          tooltip-format = "<tt>{calendar}</tt>";
+          calendar = {
+            format = {
+              today = "<span><b>{}</b></span>";
+            };
+          };
+        };
+
+        "battery" = {
+          interval = 30;
+
+          states = {
+            "good" = 75;
+            "warning" = 10;
+            "critical" = 5;
+          };
+
+          format = "{capacity}% {icon}";
+          format-charging = "{capacity}% 󰂄";
+          format-plugged = "{capacity}% ";
+          format-alt = "{time} {icon}";
+          format-icons = [
+            "󰁻"
+            "󰁼"
+            "󰁾"
+            "󰂀"
+            "󰂂"
+            "󰁹"
+          ];
+        };
       };
     };
+
+    style = ''
+      * {
+        border: none;
+        border-radius: 0;
+
+        padding: 2px 4px;
+
+        font-weight: 600;
+      }
+
+      #workspaces button {
+        border-radius: 6px;
+        border-bottom-left-radius: 0;
+        border-bottom-right-radius: 0;
+      }
+    '';
   };
 }
